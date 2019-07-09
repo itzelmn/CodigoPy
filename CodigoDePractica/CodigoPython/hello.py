40c2fd463e8e45efb4f3a6e9c07f4752473faff8
labsol <labsol@d06.innovalabsnet.net>
2019-07-09 09:50:37 -0500

    primer commit

---
+++ CodigoDePractica/CodigoPython/hello.py
@@ -0,0 +1,31 @@
+#Hola mundo!, mi primer programa (uwu)
+print("Hello cold world")
+
+x= 1                 #int 
+y= 2.5              #float 
+name= "Itzel"       #str
+is_cool= True       #bool
+ 
+#Multiple definicion 
+x, y, name, is_cool = (1,2.5,"Itzel",True)
+
+#Matematicas basicas
+a = x + y 
+
+print(x,y,name,is_cool,a) 
+
+#check the type (checar el tipo de x)
+
+print(type(x)) 
+
+#cast, e.g. x to string 
+ 
+x = str(x) #passing x
+y = int(y) #passing y
+z = float(y) 
+
+print(type(y),y)
+print(type(z),z) 
+
+
+
---
+++ CodigoDePractica/CodigoPython/string.py
@@ -0,0 +1,7 @@
+name = "Itzel" 
+age = 17
+
+#concatenate
+
+print("Hello, my name is" + Itzel + "and I am" + str(17))
+
